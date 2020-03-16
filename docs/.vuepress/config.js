eff7bb5821e2b0929df7b6cbada3baa1eb51c631
王焱辉 <wyh_19@163.com>
2020-03-16 18:40:43 +0800

    修改

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,13 @@
 module.exports = {
     base: '/Blog/',
-    title: 'wyh19文档中心',
+    title: '文档中心',
     description: 'wyh19文档中心,使用Vuepress编写，用于日常工作中的技术文档积累',
     themeConfig: {
         logo: '/assets/logo.png',
         nav: [
-            { text: '快速入门', link: '/' },
+            { text: '文档列表', link: '/' },
             {
-                text: '详细说明',
+                text: '系列文章',
                 items: [
                     {
                         text: 'MD语法',
